8b2a76862e79906dbffd691f81d6b4cfe9736b5e
Mixxx-a <Mixxx-a@users.noreply.github.com>
2023-11-23 18:42:39 +0400

    hw09: add @ NotNull validation to ids

--- hw09-springMVC/src/main/java/ru/sladkov/otus/spring/hw09/dto/BookCreateDto.java
+++ hw09-springMVC/src/main/java/ru/sladkov/otus/spring/hw09/dto/BookCreateDto.java
@@ -1,6 +1,7 @@
 package ru.sladkov.otus.spring.hw09.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record BookCreateDto(@NotBlank String title, long authorId, long genreId) {
+public record BookCreateDto(@NotBlank String title, @NotNull Long authorId, @NotNull Long genreId) {
 }
